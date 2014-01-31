7adaca7aac7894ee1af4724e3f542000badf5501
elly <eleanorz@gmail.com>
2014-01-30 16:47:36 -0800

    bike class is displaying orange squares

--- 1_basic_bike/index.php
+++ 1_basic_bike/index.php
@@ -1,12 +1,71 @@
-<?php 
+<html>
+<head>
+	<title>I want to ride my Bicyle</title>
+	<link rel="stylesheet" href="main.css.php">
+</head>
+<body>
+<?php
+
+	//idea for a project: have a bicycle race with randomly generated bicycle gif's
+    function gimmeColor()
+    {
+    	$rand = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f');
+    	$color = '#'.$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)];
+    	return $color;
+    }
+
+    function gimmePrice()
+    {
+    	return rand(90,600);
+    }
+
+    function gimmeSpeed()
+    {
+    	return rand(5,30);
+    }
+
+    function gimmeOdometer()
+    {
+    	return rand(3,300);
+    }
+
 	class Bike
 	{
+		//don't need to declare public vars as public, they are automatically
+		function __construct()
+		{
+			$this->color = gimmeColor();
+			$this->price = gimmePrice();
+			$this->speed = gimmeSpeed();
+			$this->miles = gimmeOdometer();
+			echo "<div class = 'bike'></div>";
 
+		}
+
+		public function cycle()
+		{
+			$this->miles+=15;
+			return $this->miles." total miles";
+		}
+
+		public function set_color($newcolor)
+		{
+			$this->color = $newcolor;
+		}
+
+		public function get_speed()
+		{
+			return $this->max_speed."  mph";
+		}
 	}
 
 	$obj1 = new Bike();
 	$obj2 = new Bike();
+	$obj3 = new Bike();
 
-	echo $obj1;
-	echo $obj2;
+	var_dump($obj1);
+	var_dump($obj2);
+	var_dump($obj3);
  ?>
+</body>
+</html>
---
+++ 1_basic_bike/main.css.php
@@ -0,0 +1,14 @@
+<?php 
+	echo "*{
+			color:blue;
+		}";
+
+	echo ".bike
+		{
+			width: 200px;
+			height: 200px;
+			border: 3px solid orange;
+			clear: both;
+			float: left;
+		}";
+ ?>
