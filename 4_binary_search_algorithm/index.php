14be49403a7076b83efefecf0a5706771bd0e3f6
elly <eleanorz@gmail.com>
2014-02-01 20:49:51 -0800

    started binary search algorithm, successfully dumping array of randomly generated integers

---
+++ 4_binary_search_algorithm/index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<title>
+		binary search algorithm
+	</title>
+</head>
+<body>
+
+	<?php 
+
+	$a = array();
+	for ($i=0; $i < 10000; $i++)
+	{ 
+		$n = rand(0, 10000);
+
+		$a[] = $n;
+	}
+
+	var_dump($a);
+
+	?>
+
+</body>
+</html> 
+
+
