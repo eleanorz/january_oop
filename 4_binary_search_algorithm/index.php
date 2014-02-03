6b2b96ce0d8d55b33fe3ad7f3d6e50b8d5653cd2
elly <eleanorz@gmail.com>
2014-02-03 13:54:06 -0800

    worked on the binary search tree algorithm

--- 4_binary_search_algorithm/index.php
+++ 4_binary_search_algorithm/index.php
@@ -6,17 +6,70 @@
 </head>
 <body>
 
-	<?php 
+	<?php
 
-	$a = array();
-	for ($i=0; $i < 10000; $i++)
-	{ 
-		$n = rand(0, 10000);
+	// class node
+	// {
+	// 	function __construct()
+	// 	{
+	// 		$this->mom = 
+	// 	}
+	// }
 
-		$a[] = $n;
+	class binary_tree
+	{
+		var $a = [];
+
+		function __construct($n)
+		{
+			for ($i=0; $i < $n; $i++)
+			{ 
+				$x = rand(0, $n);
+				$this->a[] = $x;
+			}
+
+			echo "I just started your custom array of ".$n." numbers, randomly generated";
+		}
+
+		function display()
+		{
+			var_dump($this->a);
+		}
+
+		function sort()
+		{
+			$t1 = time();
+
+			echo "Time one is now";
+			echo $t1."<br>";
+
+			sort($this->a);
+
+			echo "I am done sorting";
+
+			var_dump($this->a);
+
+			$t2 = time();
+			echo "time 2 is now: ";
+			echo $t2."<br>";
+			echo "The difference in times is: ";
+			echo $t2 - $t1;
+		}
+
+		function node_sort()
+		{
+			
+		}
+
+
 	}
 
-	var_dump($a);
+	$b = new binary_tree(100);
+	// $b->display();
+	$b->sort();
+
+	$c = new binary_tree(100);
+	$c->node_sort();
 
 	?>
 
